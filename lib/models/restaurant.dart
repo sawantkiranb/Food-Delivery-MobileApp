615be6782db8454def3e74274b1193ff5e237107
kiransawant1987 <kiran.b.sawant@gmail.com>
2020-07-27 00:54:21 +0530

    Completed landing screen

---
+++ lib/models/restaurant.dart
@@ -0,0 +1,9 @@
+class Restaurant {
+  final String id;
+  final String name;
+  final String rating;
+  final String duration;
+  final String imageUrl;
+
+  Restaurant({this.id, this.name, this.rating, this.duration, this.imageUrl});
+}
